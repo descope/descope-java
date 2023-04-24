5cd130060b2cb55bc29775ef593a4e338740b7c5
Keshavram Kuduwa <keshavram.kuduwa@apptware.com>
2023-04-24 19:30:23 +0530

    Add signupOrIn, update user email and phone to Magiclink

--- src/main/java/com/descope/literals/Routes.java
+++ src/main/java/com/descope/literals/Routes.java
@@ -4,12 +4,15 @@ import lombok.experimental.UtilityClass;
 
 public class Routes {
 
-  @UtilityClass
-  public static class AuthEndPoints {
-    public static final String SIGN_IN_MAGIC_LINK = "/auth/magiclink/signin";
-    public static final String SIGN_UP_MAGIC_LINK = "/auth/magiclink/signup";
-    public static final String VERIFY_MAGIC_LINK = "/auth/magiclink/verify";
+    @UtilityClass
+    public static class AuthEndPoints {
+        public static final String SIGN_IN_MAGIC_LINK = "/auth/magiclink/signin";
+        public static final String SIGN_UP_MAGIC_LINK = "/auth/magiclink/signup";
+        public static final String VERIFY_MAGIC_LINK = "/auth/magiclink/verify";
+        public static final String SIGN_UP_OR_IN_MAGIC_LINK = "auth/magiclink/signup-in";
+        public static final String UPDATE_EMAIL_MAGIC_LINK = "auth/magiclink/update/email";
+        public static final String UPDATE_USER_PHONE_MAGIC_LINK = "auth/magiclink/update/phone";
 
-    public static final String GET_KEYS_LINK = "/keys";
-  }
+        public static final String GET_KEYS_LINK = "/keys";
+    }
 }
