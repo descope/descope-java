2e4e15dad6b19a9bc927138a34c946bd27bf07cd
kuduwa-keshavram <131107576+kuduwa-keshavram@users.noreply.github.com>
2023-04-25 12:33:11 +0530

    1 | New: Introduced Mockito (#2)
    Co-authored-by: Keshavram Kuduwa <keshavram.kuduwa@apptware.com>

--- src/main/java/com/descope/model/jwt/SigningKey.java
+++ src/main/java/com/descope/model/jwt/SigningKey.java
@@ -1,10 +1,14 @@
 package com.descope.model.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class SigningKey {
   private String alg;
   private String e;
---
+++ src/main/java/com/descope/utils/JwtUtils.java
@@ -0,0 +1,35 @@
+package com.descope.utils;
+
+import com.descope.model.jwt.Token;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.Jwts;
+import lombok.experimental.UtilityClass;
+
+import java.security.Key;
+import java.util.concurrent.TimeUnit;
+
+
+@UtilityClass
+public class JwtUtils {
+    private static final long SKEW_SECONDS = TimeUnit.SECONDS.toSeconds(5);
+
+    public static Token getToken(String jwt, Key key) {
+        var jwtParser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .setAllowedClockSkewSeconds(SKEW_SECONDS)
+                .build();
+        Jws<Claims> claimsJws = jwtParser.parseClaimsJws(jwt);
+        JwsHeader<?> header = claimsJws.getHeader();
+        var claims = claimsJws.getBody();
+
+        return Token.builder()
+                .jwt(jwt)
+                .projectId(header.getKeyId())
+                .id(claims.getId())
+                .expiration(claims.getExpiration().getTime())
+                .claims(claims)
+                .build();
+    }
+}
