83a11fc68e5bd93ed2224fecac4cf7893b024e79
Keshavram Kuduwa <keshavram.kuduwa@apptware.com>
2023-06-01 16:46:57 +0530

    Refactored: Formatted Code

--- src/main/java/com/descope/sdk/auth/PasswordService.java
+++ src/main/java/com/descope/sdk/auth/PasswordService.java
@@ -7,28 +7,16 @@ import com.descope.model.user.User;
 
 public interface PasswordService {
 
-  AuthenticationInfo signUp(
-      String loginId,
-      User user,
-      String password)
+  AuthenticationInfo signUp(String loginId, User user, String password) throws DescopeException;
+
+  AuthenticationInfo signIn(String loginId, String password) throws DescopeException;
+
+  void sendPasswordReset(String loginId, String redirectURL) throws DescopeException;
+
+  void updateUserPassword(String loginId, String newPassword) throws DescopeException;
+
+  void replaceUserPassword(String loginId, String oldPassword, String newPassword)
       throws DescopeException;
-
-  AuthenticationInfo signIn(
-      String loginId,
-      String password) throws DescopeException;
-
-  void sendPasswordReset(
-      String loginId,
-      String redirectURL) throws DescopeException;
-
-  void updateUserPassword(
-      String loginId,
-      String newPassword) throws DescopeException;
-
-  void replaceUserPassword(
-      String loginId,
-      String oldPassword,
-      String newPassword) throws DescopeException;
 
   PasswordPolicy getPasswordPolicy() throws DescopeException;
 }
