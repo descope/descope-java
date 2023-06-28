160a246be832a7ff7054420f86164e29172eff99
Keshavram Kuduwa <keshavram.kuduwa@apptware.com>
2023-06-28 20:16:55 +0530

    Update management cli

--- examples/management-cli/src/main/java/com/descope/ManagementCLI.java
+++ examples/management-cli/src/main/java/com/descope/ManagementCLI.java
@@ -91,6 +91,175 @@ public class ManagementCLI {
             }
             case "user-search-all" -> cliService.searchAllUsers();
 
+            case "access-key-create" -> {
+                String keyName = System.getProperty("keyName");
+                String expirationTime = System.getProperty("expirationTime");
+
+                if (StringUtils.isAnyBlank(keyName, expirationTime)) {
+                    throw new IllegalArgumentException(
+                            "Key Name and Expiration Time are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+
+                cliService.createAccessKey(keyName, expirationTime, null, null);
+            }
+            case "access-key-update" -> {
+                String keyId = System.getProperty("keyId");
+                String keyName = System.getProperty("keyName");
+
+                if (StringUtils.isAnyBlank(keyId, keyName)) {
+                    throw new IllegalArgumentException(
+                            "Key ID, Key Name are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.updateAccessKey(keyId, keyName);
+            }
+            case "access-key-delete" -> {
+                String keyId = System.getProperty("keyId");
+
+                if (StringUtils.isAnyBlank(keyId)) {
+                    throw new IllegalArgumentException(
+                            "Key ID is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.deleteAccessKey(keyId);
+
+            }
+            case "access-key-load" -> {
+                String keyId = System.getProperty("keyId");
+
+                if (StringUtils.isAnyBlank(keyId)) {
+                    throw new IllegalArgumentException(
+                            "Key ID is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.loadAccessKey(keyId);
+
+            }
+            case "access-key-search-all" -> cliService.searchAllAccessKey();
+
+            case "tenant-create" -> {
+                String tenantName = System.getProperty("tenantName");
+
+                if (StringUtils.isAnyBlank(tenantName)) {
+                    throw new IllegalArgumentException(
+                            "Tenant Name is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.createTenant(tenantName);
+
+            }
+            case "tenant-update" -> {
+                String tenantId = System.getProperty("tenantId");
+                String tenantName = System.getProperty("tenantName");
+
+                if (StringUtils.isAnyBlank(tenantId, tenantName)) {
+                    throw new IllegalArgumentException(
+                            "Tenant ID, Tenant Name are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.updateTenant(tenantId, tenantName);
+            }
+            case "tenant-delete" -> {
+                String tenantId = System.getProperty("tenantId");
+
+                if (StringUtils.isAnyBlank(tenantId)) {
+                    throw new IllegalArgumentException(
+                            "Tenant ID is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.deleteTenant(tenantId);
+            }
+            case "tenant-search-all" -> cliService.searchAllTenant();
+
+            case "group-all-for-tenant" -> {
+                String tenantId = System.getProperty("tenantId");
+
+                if (StringUtils.isAnyBlank(tenantId)) {
+                    throw new IllegalArgumentException(
+                            "Tenant Id is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.groupAllForTenant(tenantId);
+            }
+
+            case "group-all-for-member" -> {
+                String tenantId = System.getProperty("tenantId");
+                String userIds = System.getProperty("userIds");
+                String loginIds = System.getProperty("loginIds");
+
+                if (StringUtils.isAnyBlank(tenantId, userIds, loginIds)) {
+                    throw new IllegalArgumentException(
+                            "Tenant ID, userIds and loginIds are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.groupAllForMember(tenantId, userIds, loginIds);
+            }
+            case "group-members" -> {
+                String tenantId = System.getProperty("tenantId");
+                String groupId = System.getProperty("groupId");
+
+                if (StringUtils.isAnyBlank(tenantId, groupId)) {
+                    throw new IllegalArgumentException(
+                            "Tenant ID, Group Id are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.groupMembers(tenantId, groupId);
+            }
+
+
+            case "permission-create" -> {
+                String permissionName = System.getProperty("permissionName");
+
+                if (StringUtils.isAnyBlank(permissionName)) {
+                    throw new IllegalArgumentException(
+                            "Permission Name Is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+
+                cliService.createPermission(permissionName);
+            }
+            case "permission-update" -> {
+                String permissionName = System.getProperty("permissionName");
+                String permissionId = System.getProperty("permissionId");
+
+                if (StringUtils.isAnyBlank(permissionName, permissionId)) {
+                    throw new IllegalArgumentException(
+                            "Permission Name, Permission Id are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.updatePermission(permissionName, permissionId);
+            }
+            case "permission-delete" -> {
+                String permissionId = System.getProperty("permissionId");
+
+                if (StringUtils.isAnyBlank(permissionId)) {
+                    throw new IllegalArgumentException(
+                            "Permission Id is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.deletePermission(permissionId);
+            }
+            case "permission-all" -> cliService.permissionAll();
+
+            case "role-create" -> {
+                String roleName = System.getProperty("roleName");
+
+                if (StringUtils.isAnyBlank(roleName)) {
+                    throw new IllegalArgumentException(
+                            "Role Name Is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+
+                cliService.createRole(roleName);
+            }
+            case "role-update" -> {
+                String roleName = System.getProperty("roleName");
+                String roleId = System.getProperty("roleId");
+
+                if (StringUtils.isAnyBlank(roleId, roleName)) {
+                    throw new IllegalArgumentException(
+                            "Role Name, Role Id are Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.updateRole(roleId, roleName);
+            }
+            case "role-delete" -> {
+                String roleId = System.getProperty("roleId");
+
+                if (StringUtils.isAnyBlank(roleId)) {
+                    throw new IllegalArgumentException(
+                            "Role Id is Required. Please pass these arguments as `-D<key>=<value>`");
+                }
+                cliService.deleteRole(roleId);
+            }
+            case "role-all" -> cliService.roleAll();
+            
             default -> throw new UnsupportedOperationException(
                     "Invalid Operation %s".formatted(operation));
         }
