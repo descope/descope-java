23b4954d6abf03df1589bc32734ba12767f6215f
aviadl <aviad@descope.com>
2023-10-03 14:24:28 +0300

    Linting fixes

--- src/main/java/com/descope/model/tenant/request/TenantSearchRequest.java
+++ src/main/java/com/descope/model/tenant/request/TenantSearchRequest.java
@@ -1,10 +1,8 @@
 package com.descope.model.tenant.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.Map;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
