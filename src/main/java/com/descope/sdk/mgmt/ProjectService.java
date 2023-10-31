27dbb50bc1dd5be346910edaddb2ad33bd94f50e
Asaf Shen <asaf@descope.com>
2023-10-31 14:58:40 +0200

    checkstyle

--- src/main/java/com/descope/sdk/mgmt/ProjectService.java
+++ src/main/java/com/descope/sdk/mgmt/ProjectService.java
@@ -5,22 +5,22 @@ import com.descope.exception.DescopeException;
 import com.descope.model.project.NewProjectResponse;
 
 public interface ProjectService {
-	/**
-	 * Update the current project name.
-	 *
-	 * @param name The new name for the project
- 	 * @throws DescopeException - error upon failure
-	 */
-	void updateName(String name) throws DescopeException;
+  /**
+   * Update the current project name.
+   *
+   * @param name The new name for the project
+   * @throws DescopeException - error upon failure
+   */
+  void updateName(String name) throws DescopeException;
 
-	/**
-	 * Clone the current project, including its settings and configurations
-	 * Users, tenants and access keys are not cloned.
-	 *
-	 * @param name The new name for the project
-	 * @param tag  The tag for the project
-	 * @return {@link NewProjectResponse NewProjectResponse}
-	 * @throws DescopeException - error upon failure
-	 */
-	NewProjectResponse clone(String name, ProjectTag tag) throws DescopeException;
+  /**
+   * Clone the current project, including its settings and configurations
+   * Users, tenants and access keys are not cloned.
+   *
+   * @param name The new name for the project
+   * @param tag  The tag for the project
+   * @return {@link NewProjectResponse NewProjectResponse}
+   * @throws DescopeException - error upon failure
+   */
+  NewProjectResponse clone(String name, ProjectTag tag) throws DescopeException;
 }
