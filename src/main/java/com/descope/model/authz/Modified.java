e9eb5d74f6df6b4365b9bf458beee8882dbd196b
Slavik Markovich <s@descope.com>
2024-01-22 15:24:54 -0800

    Missing file

---
+++ src/main/java/com/descope/model/authz/Modified.java
@@ -0,0 +1,20 @@
+package com.descope.model.authz;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class Modified {
+  List<String> resources;
+  List<String> targets;
+  boolean schemaChanged;
+}
