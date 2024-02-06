ffd73438add36a0cc87e79b870e2ffac01a7ee7a
Gil Shapira <gshapira@gmail.com>
2024-02-06 13:14:22 +0200

    Cleanup clone project response

--- src/main/java/com/descope/model/project/NewProjectResponse.java
+++ src/main/java/com/descope/model/project/NewProjectResponse.java
@@ -1,6 +1,5 @@
 package com.descope.model.project;
 
-import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,17 +12,5 @@ import lombok.NoArgsConstructor;
 public class NewProjectResponse {
   private String projectId;
   private String projectName;
-  private Map<String, Object> projectSettingsWeb;
-  private Map<String, Object> authMethodsMagicLink;
-  private Map<String, Object> authMethodsOTP;
-  private Map<String, Object> authMethodsSAML;
-  private Map<String, Object> authMethodsOAuth;
-  private Map<String, Object> authMethodsWebAuthn;
-  private Map<String, Object> authMethodsTOTP;
-  private Map<String, Object> messagingProvidersWeb;
-  private Map<String, Object> authMethodsEnchantedLink;
-  private Map<String, Object> authMethodsPassword;
-  private Map<String, Object> authMethodsOIDCIDP;
-  private Map<String, Object> authMethodsEmbeddedLink;
   private String tag;
 }
