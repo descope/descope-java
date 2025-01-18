66c93aab25299d8c82f942d3875abd5acdcfdccc
Aviad Lichtenstadt <aviad@descope.com>
2025-01-18 20:14:06 +0200

    Add template ID for invite options (#147)

--- src/main/java/com/descope/model/auth/InviteOptions.java
+++ src/main/java/com/descope/model/auth/InviteOptions.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InviteOptions {
   private String inviteUrl;
+  private String templateId;
   private Boolean sendEmail;
   private Boolean sendSMS;
 }
